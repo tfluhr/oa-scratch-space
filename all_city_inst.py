57dbba825d3e3f372406d67bbcad50e9cb9c5550
smurphy13IIT <110050300+smurphy13IIT@users.noreply.github.com>
2022-07-26 11:51:16 -0500

    Update all_city_inst.py

--- all_city_inst.py
+++ all_city_inst.py
@@ -12,76 +12,81 @@ def all_city_institutions(header):
     ## output directory
     openalex_dir = r'C:\tmp\openalex'
 
-    ## ask for a country name; convert it to uppercase letters
-    ## then retrieve the ISO country code
-    ## and feed it to the API call
-    country_input = input( 'Enter a country to search: ' )
-    if country_input == 'quit':
-        exit()
-    country_conv = CC[country_input.upper().strip('\u200e')]
-    country = country_conv.lower()
-
-
-    ## first API call to get the number of results and pages
-    apicall = ('https://api.openalex.org/institutions?filter=country_code:{}&per_page=200&{}&cursor=*'.format(country, header, cursor))
-    works_count = (requests.get(apicall).json()['meta']['count'])
-    page_count = math.ceil(works_count/200)
-
-    ## create a folder for output files if one doesn't exist
-    isExist = os.path.exists(openalex_dir)
-    if not isExist:
-        os.makedirs(openalex_dir)
-        print("Created new directory for your files")
-
-    ## main loop
     while True:
-
-        ## reset the cursor key
-        cursor = "*"
-
-        ## empty lists that will contain institution names
-        inst_list = []
-        inst_list_file = []
-
-        ## ask for a city name
-        city = input("Enter city for a list of institutions: ")
-        if city == 'quit':
+        
+        ## ask for a country name; convert it to uppercase letters
+        ## then retrieve the ISO country code
+        ## and feed it to the API call
+        country_input = input( 'Enter a country to search: ' )
+        if country_input == 'quit':
             exit()
+        country_conv = CC[country_input.upper().strip('\u200e')]
+        country = country_conv.lower()
 
-        ## loop to make an API call for each page of results from the initial country query
-        ## save institution names that match the city query in a list object
-        for i in range(1, page_count):
-            apicall = (
-                'https://api.openalex.org/institutions?filter=country_code:{}&per_page=200&{}&cursor={}'.format(
-                    country, header, cursor))
-            f = urllib.request.urlopen(apicall)
-            x = json.load(f)
 
-            ## set the cursor to pull the next page of results
-            cursor = x['meta']['next_cursor']
+        ## first API call to get the number of results and pages
+        apicall = ('https://api.openalex.org/institutions?filter=country_code:{}&per_page=200&{}&cursor=*'.format(country, header, cursor))
+        works_count = (requests.get(apicall).json()['meta']['count'])
+        page_count = math.ceil(works_count/200)
 
-            ## check the results for matches to the city query and add them to the list object
-            for i in x['results']:
-                if i['geo']['city'] == city:
-                    inst_list.append(i['display_name'])
+        ## create a folder for output files if one doesn't exist
+        isExist = os.path.exists(openalex_dir)
+        if not isExist:
+            os.makedirs(openalex_dir)
+            print("Created new directory for your files")
 
-            ## close the JSON file when all matching results have been extracted
-            f.close()
+        ## main loop
+        while True:
 
-        ## print the total number of results
-        print('Number of Institutions in ' + city + ': ' + str(len(inst_list)))
+            ## reset the cursor key
+            cursor = "*"
 
-        ## save the entries in the main list to a new list that includes carriage returns for
-        ## output to .txt
-        for i in inst_list:
-            inst_list_file.append(str(i) + "\n")
-            print(i)
-        print("\n")
+            ## empty lists that will contain institution names
+            inst_list = []
+            inst_list_file = []
 
-        ## save the contents of the list to a .txt file named for the city query
-        with open(openalex_dir + "\\" + str(city) + ".txt", 'w') as file:
-            file.writelines('Number of Institutions in ' + city + ': ' + str(len(inst_list_file)) + '\n\n')
-            file.writelines(inst_list_file)
+            ## ask for a city name
+            city = input("Enter city for a list of institutions: ")
+            if city == 'new country':
+                break
+
+            if city == 'quit':
+                exit()
+
+            ## loop to make an API call for each page of results from the initial country query
+            ## save institution names that match the city query in a list object
+            for i in range(1, page_count):
+                apicall = (
+                    'https://api.openalex.org/institutions?filter=country_code:{}&per_page=200&{}&cursor={}'.format(
+                        country, header, cursor))
+                f = urllib.request.urlopen(apicall)
+                x = json.load(f)
+
+                ## set the cursor to pull the next page of results
+                cursor = x['meta']['next_cursor']
+
+                ## check the results for matches to the city query and add them to the list object
+                for i in x['results']:
+                    if i['geo']['city'] == city:
+                        inst_list.append(i['display_name'])
+
+                ## close the JSON file when all matching results have been extracted
+                f.close()
+
+            ## print the total number of results
+            print('Number of Institutions in ' + city + ': ' + str(len(inst_list)))
+
+            ## save the entries in the main list to a new list that includes carriage returns for
+            ## output to .txt
+            for i in inst_list:
+                inst_list_file.append(str(i) + "\n")
+                print(i)
+            print("\n")
+
+            ## save the contents of the list to a .txt file named for the city query
+            with open(openalex_dir + "\\" + str(city) + ".txt", 'w') as file:
+                file.writelines('Number of Institutions in ' + city + ': ' + str(len(inst_list_file)) + '\n\n')
+                file.writelines(inst_list_file)
 
 
 all_city_inst = all_city_institutions('mailto=smurphy13@iit.edu')
