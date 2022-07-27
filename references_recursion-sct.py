b1948cfe9ac727bb671d25eae969f37e25e3827a
Sarah C Thorngate <sct8690@library.northwestern.edu>
2022-07-27 10:01:59 -0500

    get rid of duplicate api call

--- references_recursion-sct.py
+++ references_recursion-sct.py
@@ -2,28 +2,23 @@
 import requests, json, urllib, os, sys, math, pandas
 
 header = "mailto=sarah.thorngate@gmail.com"
-oaid = "https://openalex.org/W2073788864"
+seed = "https://openalex.org/W2073788864"
 big_df = pandas.DataFrame(columns=['OAID', 'count', 'references'])
-nope_list = [oaid]
+nope_list = []
 
 def get_work(oaid):
     ## Construct API Call
     apicall = ('https://api.openalex.org/works/{}{}'.format(oaid, header))
-    #  Open api call url and assign to variabl
+    #  Open api call url and assign to variable
     dirtywork = urllib.request.urlopen(apicall)
     #  Convert to json
     work = json.load(dirtywork)
     print(apicall)
     return work
 
-def check_references(work):
+def check_references(reflist):
     global big_df
-    # Some logic - if there is some references work, add to big list
-    if work['referenced_works']:
-        print("there are some referenced works")
-    else:
-        print("no more references") 
-    for r in work['referenced_works']:
+    for r in reflist:
         if r not in big_df.OAID.tolist() and r not in nope_list:
             rwork = get_work(r)
             if rwork['cited_by_count'] > 2000:
@@ -33,11 +28,15 @@ def check_references(work):
             else:
                 nope_list.append(r)
                 print(len(nope_list))
-                
 
-for i in big_df.OAID.tolist():
-    work = get_work(i)
-    check_references(work)
+#get things started with the seed document                
+work = get_work(seed)
+check_references(work['referenced_works'])
+
+#then loop through each work's references
+for i in big_df.references.tolist():
+    check_references(i)
+
 
 
 
